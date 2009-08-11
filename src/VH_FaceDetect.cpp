3d605868028796c7ddf32415000e13cc460b5da0
Vilya Harvey <vilya@kamaji.local>
2009-08-11 02:34:51 +0100

    Removed the downsampling step from the detect_and_draw function.
    The OpenCV book says it's optional, so that's good enough for me. :-)
    That means we have to do less allocation and copying, so should
    hopefully make things a bit faster (although it does mean we're
    processing more pixels...).

--- src/VH_FaceDetect.cpp
+++ src/VH_FaceDetect.cpp
@@ -11,6 +11,17 @@ static const char* const HELP = "Detects faces using the OpenCV library.";
 
 using namespace DD::Image;
 
+//
+// DECLARATIONS
+//
+
+struct span {
+    int left;
+    int right;
+
+    span(int _left, int _right) : left(_left), right(_right) {}
+};
+
 
 //
 // PUBLIC METHODS
@@ -95,7 +106,6 @@ void VH_FaceDetect::_open() {
         error("Unable to load the source image.");
         return;
     }
-    //print_image_info();
 
     if (m_cascadeFile != NULL && strlen(m_cascadeFile) > 0) {
         m_cascade = (CvHaarClassifierCascade *)cvLoad(m_cascadeFile, m_storage, 0, 0);
@@ -106,51 +116,42 @@ void VH_FaceDetect::_open() {
         }
     }
 
-    if (m_img != NULL && m_cascade != NULL) {
+    if (m_img != NULL && m_cascade != NULL)
         detect_and_draw(m_img);
-    }
 }
 
 
-struct span {
-    int left;
-    int right;
-
-    span(int _left, int _right) : left(_left), right(_right) {}
-};
-
-
-void VH_FaceDetect::engine(int y, int x, int r, ChannelMask channels, Row& row) {
-    Row in(x, r);
-    in.get(input0(), y, x, r, channels);
+void VH_FaceDetect::engine(int y, int left, int right, ChannelMask channels, Row& row) {
+    Row in(left, right);
+    in.get(input0(), y, left, right, channels);
 
     if (m_faces != NULL) {
         std::vector<span> spans;
-        for (int f = 0; f < (m_faces ? m_faces->total : 0); ++f) {
-            CvRect* rect = (CvRect*)cvGetSeqElem(m_faces, f);
+        for (int face = 0; face < (m_faces ? m_faces->total : 0); ++face) {
+            CvRect* rect = (CvRect*)cvGetSeqElem(m_faces, face);
             if (rect->y <= y && (rect->y + rect->height) > y)
-                if (rect->x <= r && (rect->x + rect->width) > x)
+                if (rect->x <= right && (rect->x + rect->width) > left)
                     spans.push_back(span(rect->x, rect->x + rect->width));
         }
 
         foreach(chan, channels) {
             float *out = row.writable(chan);
-            for (int i = x; i < r; ++i) {
+            for (int x = left; x < right; ++x) {
                 std::vector<span>::const_iterator s;
                 bool faceCount = 0;
                 for (s = spans.begin(); s != spans.end(); s++) {
-                    if (s->left <= i && i < s->right)
+                    if (s->left <= x && x < s->right)
                         ++faceCount;
                 }
                 double faceScale = (faceCount + 1.0) / (m_faces->total + 1.0);
-                out[i] = in[chan][i] * faceScale;
+                out[x] = in[chan][x] * faceScale;
             }
         }
     } else {
         foreach(chan, channels) {
             float *out = row.writable(chan);
-            for (int i = x; i < r; ++i)
-                out[i] = in[chan][i];
+            for (int x = left; x < right; ++x)
+                out[x] = in[chan][x];
         }
     }
 }
@@ -202,50 +203,25 @@ IplImage *VH_FaceDetect::build_opencv_image() const {
 }
 
 
-void VH_FaceDetect::print_image_info() const {
-    if (m_img != NULL) {
-        fprintf(stderr, "Loaded image:\n");
-        fprintf(stderr, "    # channels:    %d\n", m_img->nChannels);
-        fprintf(stderr, "    alpha channel: %d\n", m_img->alphaChannel);
-        fprintf(stderr, "    depth:         %d\n", m_img->depth);
-        fprintf(stderr, "    data order:    %d\n", m_img->dataOrder);
-        fprintf(stderr, "    origin:        %d\n", m_img->origin);
-        fprintf(stderr, "    align:         %d\n", m_img->align);
-        fprintf(stderr, "    width:         %d\n", m_img->width);
-        fprintf(stderr, "    height:        %d\n", m_img->height);
-        fprintf(stderr, "    width step:    %d\n", m_img->widthStep);
-    }
-}
-
-
 void VH_FaceDetect::detect_and_draw(IplImage *img) {
     if (m_cascade != NULL) {
-        double scale = 1.3;
         IplImage* gray = cvCreateImage(cvSize(img->width,img->height), 8, 1);
-        IplImage* small_img = cvCreateImage(
-                cvSize(cvRound (img->width/scale), cvRound (img->height/scale)),
-                8, 1);
 
         cvCvtColor(img, gray, CV_BGR2GRAY);
-        cvResize(gray, small_img, CV_INTER_LINEAR);
-        cvEqualizeHist(small_img, small_img);
+        cvEqualizeHist(gray, gray);
+        cvEqualizeHist(gray, gray);
         cvClearMemStorage(m_storage);
         m_faces = NULL;
 
-        m_faces = cvHaarDetectObjects( small_img, m_cascade, m_storage,
+        m_faces = cvHaarDetectObjects( gray, m_cascade, m_storage,
                                        1.1, 2, 0/*CV_HAAR_DO_CANNY_PRUNING*/,
                                        cvSize(30, 30) );
         for(int i = 0; i < (m_faces ? m_faces->total : 0); i++) {
-            CvRect* r = (CvRect*)cvGetSeqElem(m_faces, i );
-            r->x *= scale;
-            r->y *= scale;
-            r->width *= scale;
-            r->height *= scale;
+            CvRect* r = (CvRect*)cvGetSeqElem(m_faces, i);
             r->y = img->height - r->y - r->height;
         }
 
         cvReleaseImage( &gray );
-        cvReleaseImage( &small_img );
     }
 }
 
